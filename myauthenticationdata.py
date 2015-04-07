32582958ab045f088399c39851173ced098c82af
Linda Feng <lindafeng115@gmail.com>
2015-04-06 22:16:09 -0400

    changed authenticationlocation

---
+++ myauthenticationdata.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Hidden authentication data for the Amadeus Code exercises
+** Not for github **
+"""
+
+path = "https://export.airconomy.com/candidates/"
+dir_user = "candidates"
+dir_passwd = "7!poTNja09ljC"
+
+
